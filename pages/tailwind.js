ae8543c98513fe73521a3be46cef2a86d1730701
Steven Soewignyo <54796666+stevensa1@users.noreply.github.com>
2022-04-03 12:39:32 +0700

    Update Navigation

---
+++ pages/tailwind.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useRef, useState} from 'react'
+
+const tailwind = ({ fixed }) => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <>
+            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                      <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                        href="#pablo">
+                        pink Tailwind Starter Kit
+                      </a>
+                      <button
+                        className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                        type="button"
+                        onClick={() => setNavbarOpen(!navbarOpen)}>
+                        <svg className='w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>></svg>
+                        <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                      </button>
+                    </div>
+                    <div className={"lg:flex flex-grow items-center transition" + (navbarOpen ? " flex" : " hidden")}
+                        id="example-navbar-danger">
+                      <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                        <li className="nav-item">
+                          <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                            href="#pablo">
+                            <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                          </a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                            href="#pablo">
+                            <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+            </>
+        </div>
+  )
+}
+
+export default tailwind
